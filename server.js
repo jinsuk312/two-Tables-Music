feb6c40f495e6c2159e9db6788219bccfae7b1cf
jinsukmik <jinsukmik@gmail.com>
2019-02-03 21:27:21 -0600

    added new queries for deleting data in the top5000 table

--- server.js
+++ server.js
@@ -34,10 +34,14 @@ function runSearch() {
             message: "What would you like to do?",
             choices: [
                 "Find songs by artist",
+                "Delete songs by artist name",
                 "Find all artists who appear more than once",
+                "Delete all artist who appear only once",
                 "Find data within a specific range",
+                "Delete data within a specific range",
                 "Search for a specific song",
                 "Find artists with a top song and top album in the same year"
+                
             ]
         })
         // where we use the user choice to run the next function and break out of the switch statement
@@ -57,6 +61,15 @@ function runSearch() {
                     break;
                 case "Find artists with a top song and top album in the same year":
                     songAndAlbumSearch();
+                    break;
+                case "Delete songs by artist name":
+                    deleteSongByArtist();
+                    break;
+                case "Delete data within a specific range":
+                    deleteSongsByRange();
+                    break;
+                case "Delete all artist who appear only once":
+                    singleSearch();
                     break;
             }
         })
@@ -174,7 +187,7 @@ function songSearch() {
             })
         })
 }
-// function to handle search for matches with top songs in the same year of top albums
+// function to handle search for matches with top songs in the same year of top albums //
 var songAndAlbumSearch = function(){
     inquirer
     .prompt({
@@ -182,17 +195,95 @@ var songAndAlbumSearch = function(){
         type:"input",
         message: "What artist would you like to look for?"
     })
+    // use user input to create a SQL query
     .then(function(answer){
-        var query = "SELECT top_albums.year, top_albums.position, top_albums.position, top5000.song, top5000.artist FROM top_albums ";
+        // create a SQL query to select matching data from the top5000 and top_albums tables by using an INNER JOIN on similar properties
+        //
+        var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist FROM top_albums ";
         query += "INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year = top5000.year) ";
         query += "WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year ";
-        
+        // sends our query and replace ? ? with our answer.artist and answer.artist
         connection.query(query,[answer.artist,answer.artist], function(err,res){
+            // console.log the iteration of responses and their proprties position, artist, song,album and year 
             console.log(res.length + " Matches Found!");
             for(var i = 0; i < res.length; i++){
                 console.log("Album Position: " + res[i].position + "\nArist: " + res[i].artist + "\nSong: " + res[i].song + "\nAlbum: " + res[i].album + "\nYear: " + res[i].year + "\n-------");
             }
+            // we bring the user back to the beginning and prompt the user again
             runSearch();
         })
     })
+}
+// function to handle deleting songs by artist name //
+var deleteSongByArtist = function(){
+    inquirer
+    .prompt({
+        name: "artist",
+        type: "input",
+        message: "Which artist's songs would you like to delete?"
+    })
+    //use user input to create a SQL query
+    .then(function(answer){
+        // send our query and replace ? with our answer.artist
+        var query = "DELETE FROM top5000 WHERE top5000.artist = ? ";
+        connection.query(query,[answer.artist], function(err,res){
+            console.log("Deleted all songs by artist name: " + answer.artist + ".");
+        })
+        runSearch();
+    })
+}
+// function that handles data deletion by position range
+var deleteSongsByRange = function(){
+    inquirer
+        // where we ask for the position range for songs to delete for
+    .prompt([
+        {
+            name: "start",
+            type: "input",
+            message: "Entering starting positions: ",
+            // check to see if value is not a number 
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false
+            }
+        },
+        {
+            name: "end",
+            type: "input",
+            message: "Enter ending positions: ",
+            // check to see if the value is not a number
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        }
+    ])
+    // uses user input to create a SQL query
+    .then(function (answer) {
+        // create a SQL query to delete from the table top500 where position is between answer.start and answer.end
+        var query = "DELETE FROM top5000 WHERE position BETWEEN ? AND ?";
+        //send our query and replace ? ? with answer.start and answer.end
+        connection.query(query, [answer.start, answer.end], function (err, res) {
+                console.log("Deleted data from position " + answer.start + " to position " + answer.end + ".");
+            // we bring the user back to the beginning and prompt the user again
+            runSearch();
+        })
+    })
+}
+//function to handle looking for artists that appear more than once //
+function singleSearch() {
+    // create query to select artist from the table top5000 that show more than once
+    var query = 'SELECT artist from top5000 GROUP BY artist HAVING count(*) = 1';
+    connection.query(query, function (err, res) {
+        // iterate through the response and console.log each of their artist names
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].artist);
+        }
+        // take the user back to the beginning and prompt the user again
+        runSearch();
+    });
 }
